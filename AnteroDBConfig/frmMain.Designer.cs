547b2429b1df95253b7e84e1d6a23ae7b31b42f8
Justin Oberdorf <joberdorf@acs-ilm.com>
2019-10-22 09:38:12 -0400

    Add DB Config Tool

---
+++ AnteroDBConfig/frmMain.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace AnteroDBConfig
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDatabase = new System.Windows.Forms.TextBox();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnPassReveal = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.cmbAuthMethod = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtPOTD = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtDatabase);
+            this.groupBox1.Controls.Add(this.txtServer);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(382, 134);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Server";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Database:";
+            // 
+            // txtDatabase
+            // 
+            this.txtDatabase.Location = new System.Drawing.Point(100, 77);
+            this.txtDatabase.Name = "txtDatabase";
+            this.txtDatabase.Size = new System.Drawing.Size(273, 20);
+            this.txtDatabase.TabIndex = 2;
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(100, 42);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(273, 20);
+            this.txtServer.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Host:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnPassReveal);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtPassword);
+            this.groupBox2.Controls.Add(this.txtUsername);
+            this.groupBox2.Controls.Add(this.cmbAuthMethod);
+            this.groupBox2.Location = new System.Drawing.Point(12, 152);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(382, 170);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Authentication";
+            // 
+            // btnPassReveal
+            // 
+            this.btnPassReveal.BackColor = System.Drawing.SystemColors.Window;
+            this.btnPassReveal.BackgroundImage = global::AnteroDBConfig.Properties.Resources.passwordreveal;
+            this.btnPassReveal.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnPassReveal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPassReveal.Location = new System.Drawing.Point(336, 111);
+            this.btnPassReveal.Name = "btnPassReveal";
+            this.btnPassReveal.Size = new System.Drawing.Size(37, 26);
+            this.btnPassReveal.TabIndex = 12;
+            this.btnPassReveal.UseVisualStyleBackColor = false;
+            this.btnPassReveal.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnPassReveal_MouseDown);
+            this.btnPassReveal.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnPassReveal_MouseUp);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Password:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Username:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Method:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(100, 111);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(235, 20);
+            this.txtPassword.TabIndex = 8;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(100, 77);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(273, 20);
+            this.txtUsername.TabIndex = 7;
+            // 
+            // cmbAuthMethod
+            // 
+            this.cmbAuthMethod.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbAuthMethod.FormattingEnabled = true;
+            this.cmbAuthMethod.Items.AddRange(new object[] {
+            "Windows Integrated",
+            "SQL Server Integrated"});
+            this.cmbAuthMethod.Location = new System.Drawing.Point(100, 41);
+            this.cmbAuthMethod.Name = "cmbAuthMethod";
+            this.cmbAuthMethod.Size = new System.Drawing.Size(273, 21);
+            this.cmbAuthMethod.TabIndex = 6;
+            this.cmbAuthMethod.SelectedIndexChanged += new System.EventHandler(this.cmbAuthMethod_SelectedIndexChanged);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(57, 404);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(139, 51);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(204, 404);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(139, 51);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtPOTD);
+            this.groupBox3.Location = new System.Drawing.Point(12, 329);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(382, 69);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Password of the Day";
+            // 
+            // txtPOTD
+            // 
+            this.txtPOTD.Location = new System.Drawing.Point(7, 30);
+            this.txtPOTD.Name = "txtPOTD";
+            this.txtPOTD.Size = new System.Drawing.Size(369, 20);
+            this.txtPOTD.TabIndex = 0;
+            this.txtPOTD.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(400, 467);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Antero DB Config";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDatabase;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.ComboBox cmbAuthMethod;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnPassReveal;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtPOTD;
+    }
+}
+
---
+++ AnteroDBConfig/frmMain.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AllMax;
+using AllMax.Antero;
+
+namespace AnteroDBConfig
+{
+    public static class StringExtension
+    {
+        //don''t need this in real world!!
+        public static Boolean ContainsOnlyDigits(this String str)
+        {
+            if (str == null) throw new NullReferenceException();
+            return str.All(c => char.IsDigit(c));
+        }
+    }
+    public partial class frmMain : Form
+    {
+        private bool blnWasButtonClicked = false;
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        public enum PasswordTypeEnum
+        {
+            None,
+            Password,
+            Validated
+        }
+
+        private static uint AllMaxLicenseCRC32(char[] buffer)
+        {
+            if (buffer.Length == 0)
+            {
+                return 0u;
+            }
+            uint[] array = new uint[256];
+            for (uint num = 0u; num <= 255u; num += 1u)
+            {
+                uint num2 = num;
+                for (int i = 1; i <= 8; i++)
+                {
+                    if (num2 % 2u != 0u)
+                    {
+                        num2 = (num2 >> 1 ^ 3988292384u);
+                    }
+                    else
+                    {
+                        num2 >>= 1;
+                    }
+                }
+                array[(int)num] = num2;
+            }
+            uint num3 = uint.MaxValue;
+            uint num4 = 0u;
+            while ((ulong)num4 < (ulong)((long)buffer.Length))
+            {
+                num3 = (array[(int)((byte)(num3 & 255u) ^ (byte)buffer[(int)num4])] ^ num3 >> 8);
+                num4 += 1u;
+            }
+            return ~num3;
+        }
+
+
+        private bool PasswordTest(string _txtPassword)
+        {
+            PasswordTypeEnum _pwType = PasswordTypeEnum.None;
+            string _password = "";
+            if (!string.IsNullOrEmpty(_txtPassword))
+            {
+                string text = AllMaxLicenseCRC32(("SUPPORT" + DateTime.Today.ToString("yyyyMMdd")).ToCharArray()).ToString("X8").ToLower();
+                if (_txtPassword.Length == 8 && _txtPassword.StartsWith("allmax", StringComparison.OrdinalIgnoreCase) && _txtPassword.Substring(6).ContainsOnlyDigits() && int.Parse(_txtPassword.Substring(6)) == DateTime.Today.Day)
+                {
+                    _pwType = PasswordTypeEnum.Password;
+                    _password = text;
+                }
+                else if (this.txtPassword.Text.Equals(text, StringComparison.OrdinalIgnoreCase))
+                {
+                    _pwType = PasswordTypeEnum.Validated;
+                }
+            }
+            if (_pwType != PasswordTypeEnum.None)
+                return true;
+            return false;
+        }
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            txtPOTD.Text = AllMaxLicenseCRC32(("SUPPORT" + DateTime.Today.ToString("yyyyMMdd")).ToCharArray()).ToString("X8").ToLower();
+            //load settings
+            btnPassReveal.Height = txtPassword.Height;
+            string szServer = RegistryValues.AnteroDbLocation;
+            txtServer.Text = szServer + "," + Convert.ToString(RegistryValues.AnteroDbPort);
+            txtDatabase.Text = RegistryValues.AnteroDbName;
+            txtUsername.Text = RegistryValues.AnteroDbUsername;
+            txtPassword.Text = RegistryValues.AnteroDbPassword;
+            if (RegistryValues.AnteroDbSpecifyAuth)
+                cmbAuthMethod.SelectedIndex = 1;
+            else
+                cmbAuthMethod.SelectedIndex = 0;
+
+        }
+
+        private void btnPassReveal_MouseDown(object sender, MouseEventArgs e)
+        {
+            txtPassword.UseSystemPasswordChar = false;
+        }
+
+        private void btnPassReveal_MouseUp(object sender, MouseEventArgs e)
+        {
+            txtPassword.UseSystemPasswordChar = true;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you wish to cancel changes and exit?", "Cancel Changes?", MessageBoxButtons.YesNo) == DialogResult.No)
+               return;
+            blnWasButtonClicked = true;
+            this.Close();
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!blnWasButtonClicked)
+                if (MessageBox.Show("Are you sure you wish to cancel changes and exit?", "Cancel Changes?", MessageBoxButtons.YesNo) == DialogResult.No)
+                    e.Cancel = true;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you wish to save changes and exit?", "Save Changes?", MessageBoxButtons.YesNo) == DialogResult.No)
+                return;
+            if (cmbAuthMethod.SelectedIndex == 1)
+            {
+                if (string.IsNullOrWhiteSpace(txtUsername.Text))
+                {
+                    MessageBox.Show("Username cannot be empty in SQL Server Integrated Authentication", "Error", MessageBoxButtons.OK);
+                    return;
+                }
+                if (string.IsNullOrWhiteSpace(txtPassword.Text))
+                {
+                    MessageBox.Show("Password cannot be empty in SQL Server Integrated Authentication", "Error", MessageBoxButtons.OK);
+                    return;
+                }
+                RegistryValues.AnteroDbSpecifyAuth = true;
+                RegistryValues.AnteroDbUsername = txtUsername.Text;
+                RegistryValues.AnteroDbPassword = txtPassword.Text;
+            }
+            else
+            {
+                RegistryValues.AnteroDbSpecifyAuth = false;
+                RegistryValues.AnteroDbUsername = "";
+                RegistryValues.AnteroDbPassword = "";
+            }
+            if (txtServer.Text.Contains(","))
+            {
+                string szHostname = txtServer.Text.Split(',')[0];
+                UInt16 iPort = 0;
+                try { iPort = Convert.ToUInt16(txtServer.Text.Split(',')[1]); }
+                catch { iPort = 0;  }
+                if (iPort == 0)
+                    iPort = 1433;
+                RegistryValues.AnteroDbLocation = szHostname;
+                RegistryValues.AnteroDbPort = iPort;
+            }
+            else
+            {
+                RegistryValues.AnteroDbLocation = txtServer.Text;
+                RegistryValues.AnteroDbPort = 1433;
+            }
+            RegistryValues.AnteroDbName = txtDatabase.Text;
+            blnWasButtonClicked = true;
+            this.Close();
+        }
+
+        private void cmbAuthMethod_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbAuthMethod.SelectedIndex == 1)
+            {
+                txtUsername.Enabled = true;
+                txtPassword.Enabled = true;
+            }
+            else
+            {
+                txtUsername.Enabled = false;
+                txtPassword.Enabled = false;
+            }
+        }
+        private string CalcMD5Hash(byte[] byteInput)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                byte[] encrypted = md5Hash.ComputeHash(byteInput);
+                StringBuilder sBuilder = new StringBuilder();
+                for (int i = 0; i < encrypted.Length; i++)
+                    sBuilder.Append(encrypted[i].ToString("x2"));
+                return sBuilder.ToString();
+            }
+        }
+        private bool VerifyMD5Hash(string hash1,string hash2)
+        {
+            if (string.IsNullOrWhiteSpace(hash1) || string.IsNullOrWhiteSpace(hash2))
+                return false;
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+            if (0 == comparer.Compare(hash1, hash2))
+                return true;
+            else
+                return false;
+        }
+    }
+}
